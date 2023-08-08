0e667d52c2977e64d3457d9ba7962bf30dc0e0fc
ngyngcphu <nguyenngocphu225@gmail.com>
2023-08-08 18:47:49 +0700

    test(script): fix bug overflow,  undefined behavior and validate command.

--- db.cpp
+++ db.cpp
@@ -12,6 +12,8 @@ enum MetaCommandResult {
 
 enum PrepareResult {
     PREPARE_SUCCESS,
+    PREPARE_NEGATIVE_ID,
+    PREPARE_STRING_TOO_LONG,
     PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENT
 };
@@ -32,8 +34,19 @@ enum ExecuteResult {
 class Row {
 public:
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
+
+    Row() {
+        id = 0;
+        username[0] = '\0';
+        email[0] = '\0';
+    }
+    Row(uint32_t id, const char *username, const char *email) {
+        this->id = id;
+        strncpy(this->username, username, COLUMN_USERNAME_SIZE + 1);
+        strncpy(this->email, email, COLUMN_EMAIL_SIZE + 1);
+    }
 };
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
@@ -116,6 +129,7 @@ public:
     }
     void read_input(InputBuffer *input_buffer);
     MetaCommandResult do_meta_command(InputBuffer *input_buffer, Table *table);
+    PrepareResult prepare_insert(InputBuffer *input_buffer, Statement *statement);
     PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement);
     void close_input_buffer(InputBuffer *input_buffer) {
         delete input_buffer;
@@ -150,15 +164,32 @@ MetaCommandResult Database::do_meta_command(InputBuffer *input_buffer, Table *ta
     }
 }
 
+PrepareResult Database::prepare_insert(InputBuffer *input_buffer, Statement *statement) {
+    statement->type = STATEMENT_INSERT;
+    char *keyword = strtok(input_buffer->buffer, " ");
+    char *id_string = strtok(NULL, " ");
+    char *username = strtok(NULL, " ");
+    char *email = strtok(NULL, " ");
+    if (id_string == NULL || username == NULL || email == NULL) {
+        return PREPARE_SYNTAX_ERROR;
+    }
+    int id = strtol(id_string, (char **)NULL, 10);
+    if (id < 0) {
+        return PREPARE_NEGATIVE_ID;
+    }
+    if (strlen(username) > COLUMN_USERNAME_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE) {
+        return PREPARE_STRING_TOO_LONG;
+    }
+    statement->row_to_insert = Row(id, username, email);
+    return PREPARE_SUCCESS;
+}
+
 PrepareResult Database::prepare_statement(InputBuffer *input_buffer, Statement *statement) {
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
-        statement->type = STATEMENT_INSERT;
-        int args_assigned = std::sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
-                                    statement->row_to_insert.username, statement->row_to_insert.email);
-        if (args_assigned < 3) {
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+        return prepare_insert(input_buffer, statement);
     } else if (strncmp(input_buffer->buffer, "select", 6) == 0) {
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
@@ -214,6 +245,12 @@ void Database::run() {
         switch (prepare_statement(input_buffer, statement)) {
             case (PREPARE_SUCCESS):
                 break;
+            case (PREPARE_NEGATIVE_ID):
+                std::cout << "ID must be positive.\n";
+                continue;
+            case (PREPARE_STRING_TOO_LONG):
+                std::cout << "String is too long.\n";
+                continue;
             case (PREPARE_SYNTAX_ERROR):
                 std::cout << "Syntax error. Could not parse statement.\n";
                 continue;
